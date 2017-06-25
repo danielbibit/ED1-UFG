35996bcc7e2955499ee7aed9552425a681aea9a6
Daniel Moraes <danielbibit@gmail.com>
2017-06-25 01:19:49 -0300

    Now works with the spoj test case, but not accepted.

--- src/Exercises/PESADA09.c
+++ src/Exercises/PESADA09.c
@@ -2,19 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-void my_gets(char *string){
-	fflush(stdin);
-	fflush(stdout);
-
-	fgets(string, 1000, stdin);
-
-	if(strlen(string) > 0 && string[strlen(string) - 1] == '\n')
-		string[strlen(string - 1)] = '\0';
-
-	fflush(stdin);
-	fflush(stdout);
-}
-
 int max(int a, int b){
 	if(a > b)
 		return a;
@@ -26,13 +13,13 @@ int split_string(char *str, int* tree){
 	int i = 0;
 	char * pch;
 
-    pch = strtok (str," ");
+	pch = strtok (str," ");
 
-    while (pch != NULL){
+	while (pch != NULL){
 		tree[i] = atoi(pch);
 		pch = strtok (NULL, " ");
 		i++;
-    }
+	}
 
 	return i-1;
 }
@@ -40,15 +27,15 @@ int split_string(char *str, int* tree){
 void print_char_array(char *array){
 	int i;
 	printf("print_char_aray::");
+
 	for(i = 0; i < 1000; i++){
 		printf("%c", array[i]);
 	}
 }
 
 void print_int_array(int* array){
-	printf("\nPRINT ADDR %d", &array);
-
 	int i;
+
 	for(i = 0; i < 250; i++){
 		printf("%i ", array[i]);
 	}
@@ -63,21 +50,25 @@ void initialize_tree(int* tree){
 }
 
 int height(int* tree, int node){
-	if(tree[node] == -1)
-		return 0;
-	else
+	if(tree[node] == -1){
+		return -1;
+	}else{
 		return 1 + max(height(tree, node*2+1), height(tree, node*2+2));
+	}
 }
 
+//height(tree, node[n]==-1) should be 0
 int balance(int* tree, int node){
 	return height(tree, node*2+1) - height(tree, node*2+2);
 }
 
 int verify_AVL(int* tree){
 	int i;
-	for(i = 0; i < 100; i++){
+	for(i = 0; i < 250; i++){
 		if(tree[i] != -1){
-			if(balance(tree, i) < -1 || balance(tree, i) > 1){
+			int balance_l = balance(tree, i);
+
+			if(balance_l < -1 || balance_l > 1){
 				printf("F\n");
 				return 0;
 			}
@@ -104,7 +95,18 @@ int main(){
 		fgets(buffer, 100, stdin);
 		fflush(stdin);
 
-		split_string(buffer, tree);
+		//~~le gambs
+		char* p = buffer;
+		p++;
+		p++;
+		//end gambs
+
+		split_string(p, tree);
+
+		//printf("Root Heigt: %d\n", height(tree, 0) );
+		//printf("Root Balance: %d\n", balance(tree, 0));
+
+		//print_int_array(tree);
 
 		verify_AVL(tree);
 	}
