c9cee13eb048cddaf57ace209ce169228697404c
danielbibit <danielbibit@gmail.com>
2017-07-11 07:51:52 -0300

    Pontes09 modified

--- src/Exercises/PONTES09.py
+++ src/Exercises/PONTES09.py
@@ -2,26 +2,35 @@ import numpy as np
 
 def dijk(graph, origin, destin):
     visited = []
-    distance = []
+    distance = [None for i in range(graph.shape[0])]
+
+    for i in range(graph.shape[0]):
+        distance[i] = float('inf')
+        visited.append(i)
 
     distance[origin] = 0
 
-    while True:
-        n = -1
+    while visited:
+        current_vertex = float('inf')
+        # print 'visited:', visited
+        # print 'distance:', distance
 
-        for i in range(destin):
-            if not visited[i] and (n == -1 or distance[i] < distance[n]):
-                n = i
-        if n == -1:
-            break
+        for i in range(graph.shape[0]):
+            if distance[i] < current_vertex and i in visited:
+                current_vertex = i
 
-        visited[n] = 1
+                # print 'current_vertex:', current_vertex
 
-        for i in range(destin):
-            if(dis[i] > dis[n] + graph[n, i]):
-                dis[i] = dis[n] + graph[n, i]
+        visited.remove(current_vertex)
 
-    return 0
+        for i in range(graph.shape[0]):
+            if graph[current_vertex, i] != 0:
+                alternate_path = distance[current_vertex] + graph[current_vertex, i]
+                if alternate_path < distance[i]:
+                    distance[i] = alternate_path
+
+    # return int(distance[destin-1])
+    return int(distance[destin])
 
 buff = raw_input().split(' ')
 n, m = int(buff[0]), int(buff[1])
@@ -34,5 +43,4 @@ for i in range(m):
 
     graph[buff[0], buff[1]] = graph[buff[1], buff[0]] = buff[2]
 
-print graph
-# print dijk(graph, 0, n + 2)
+print dijk(graph, 0, n + 1)
